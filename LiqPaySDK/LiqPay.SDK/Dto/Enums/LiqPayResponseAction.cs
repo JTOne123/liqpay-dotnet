c27e17b4905645aa5d4ae1ff1b57efbd768c6937
Volodymyr <61783015+vitvitskyi@users.noreply.github.com>
2023-04-13 13:08:53 +0300

    Added "Unsubscribe" to LiqPayResponseAction

--- LiqPaySDK/LiqPay.SDK/Dto/Enums/LiqPayResponseAction.cs
+++ LiqPaySDK/LiqPay.SDK/Dto/Enums/LiqPayResponseAction.cs
@@ -19,6 +19,8 @@ namespace LiqPay.SDK.Dto.Enums
         [EnumMember(Value = "regular")]
         Regular,
         [EnumMember(Value = "paycash")]
-        Paycash
+        Paycash,
+        [EnumMember(Value = "unsubscribe")]
+        Unsubscribe,
     }
 }
